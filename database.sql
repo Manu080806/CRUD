695ab7d597a22eff29dd13031208c70d002bbb27
Manu <emanuss2006@gmail.com>
2024-08-02 16:32:53 -0300

    sudo1

--- database.sql
+++ database.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE yourdbname;
+CREATE DATABASE ge;
 
-USE yourdbname;
+USE ge;
 
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -8,3 +8,5 @@ CREATE TABLE users (
     password VARCHAR(255) NOT NULL,
     role ENUM('admin', 'user') NOT NULL
 );
+
+--!Manu
